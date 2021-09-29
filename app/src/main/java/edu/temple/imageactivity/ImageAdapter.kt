f154bf4f97ede176867f2b47a0263d7669bc3ad5
Isaac <tuj12239@temple.edu>
2021-09-29 11:34:52 -0400

    Defined resource array, added adapter functions, images show

--- app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
@@ -1,25 +1,38 @@
 package edu.temple.imageactivity
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 
-class ImageAdapter(val context: Context, _images: Array<ImageClass>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ImageAdapter(val _context: Context, _images: Array<ImageClass>) : RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
 
-    class ViewHolder(imageView: ImageView) : RecyclerView.ViewHolder(imageView) {}
+    private val context = _context
+    private val images = _images
+    private val inflater = LayoutInflater.from(context)
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val newHolder = ViewHolder()
+    class ViewHolder(_view: View) : RecyclerView.ViewHolder(_view) {
+        val view = _view
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = inflater.inflate(R.layout.imageview, null)
+        return ViewHolder(view)
     }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val gridImage = holder.view.findViewById<ImageView>(R.id.gridImage)
+        gridImage.setImageResource(images[position].resource)
+
+        //holder.view.setOnClickListener{changeMainViews(position)}
+    }
+
+    //private fun changeMainViews(index: Int) {}
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return images.size
     }
 
 }
--- app/src/main/java/edu/temple/imageactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/imageactivity/MainActivity.kt
@@ -2,6 +2,8 @@ package edu.temple.imageactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -10,8 +12,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val gridView = findViewById<RecyclerView>(R.id.gridView)
-        gridView.layoutManager = GridLayoutManager(this, 3)
+        val images = getImageArray()
 
+        val gridView = findViewById<RecyclerView>(R.id.gridView)
+        val adapter = ImageAdapter(this, images)
+        gridView.layoutManager = GridLayoutManager(this, 3)
+        findViewById<TextView>(R.id.imageLabel).text = images[0].label
+        findViewById<ImageView>(R.id.imageView).setImageResource(images[0].resource)
+        gridView.adapter = adapter
+
+    }
+
+    private fun getImageArray(): Array<ImageClass> {
+
+        return arrayOf(
+            ImageClass("Donphan", R.drawable.donphan),
+            ImageClass("Dunsparce", R.drawable.dunsparce),
+            ImageClass("Eevee", R.drawable.eevee),
+            ImageClass("Gengar", R.drawable.gengar),
+            ImageClass("Heracross", R.drawable.heracross),
+            ImageClass("Magikarp", R.drawable.magikarp),
+            ImageClass("Magnezone", R.drawable.magnezone),
+            ImageClass("Medicham", R.drawable.medicham),
+            ImageClass("Mudkip", R.drawable.mudkip),
+            ImageClass("Murkrow", R.drawable.murkrow),
+            ImageClass("Prinplup", R.drawable.prinplup),
+            ImageClass("Quagsire", R.drawable.quagsire)
+        )
     }
 }
