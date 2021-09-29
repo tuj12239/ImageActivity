0ace9d14f69ec2013856a1bd33cf37837b9a5c39
Isaac <tuj12239@temple.edu>
2021-09-29 11:04:52 -0400

    Added imageview resource

--- app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
+++ app/src/main/java/edu/temple/imageactivity/ImageAdapter.kt
@@ -1,6 +1,25 @@
 package edu.temple.imageactivity
 
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 
-class ImageAdapter() : RecyclerView.Adapter<> {
+class ImageAdapter(val context: Context, _images: Array<ImageClass>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    class ViewHolder(imageView: ImageView) : RecyclerView.ViewHolder(imageView) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val newHolder = ViewHolder()
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
